c723aeba40345b1d02c9435c90cecc4c6388ca05
Mazcod1 <mazerereand@gmail.com>
2022-10-05 08:08:55 -0700

    tsk 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,13 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-
+	
 	grid = malloc(sizeof(int *) * height);
        	
 	if (grid == NULL)
 	{
 	return (NULL);
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
