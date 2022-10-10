d66b3902a1c002364f4a8cde8135ed42eb8aa343
Mazcod1 <mazerereand@gmail.com>
2022-10-10 06:16:19 -0700

    tsk 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,10 +22,32 @@ int _strlen(char *str)
 }
 
 /**
- * _strcopy - copies a string pointed to by src ,including terminating null byteto a buffer pointed to dest.
+ * _strcopy - copies a string pointed to by src,
+ * including terminating null byte to
+ * a buffer pointed to dest.
  * @dest: The buffer storing the string copy.
  * @src: The source of string
  * Return: the new structure dog
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a dog.
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner:the owner of the dog
+ * Return: the new struct dog.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
