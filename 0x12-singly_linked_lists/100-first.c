fbbc3a7355fbf56ca5e0dbefbfc115e58a802426
Mazcod1 <mazerereand@gmail.com>
2022-10-20 19:27:18 -0700

    All tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * hare - Prints string before execution of main function.
+ */
+
+void hare(void)
+{
+	printf("Am a procoder \nAnd\n thats for real");
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - Adds node at the beginning of list_t 
+ * @head: Apoiter at head of list_t
+ * @str: Strring to be added to list_t
+ * Return: If address fails - NULL
+ * else - the locaionn of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new = NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
